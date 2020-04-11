4751cf13769913b1fb0dd137e0c6eaf6450e1f37
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-04-11 20:22:15 +0700

    Tugas 3 PHP

---
+++ tugas3_1.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 360;
+$b = 90;
+$c = $a / $b;
+
+echo "Jarak Kota A - Z = $a KM <br>";
+echo "Kecepatan Motor = $b <br>";
+echo "Waktu Tempuh = ? <br>";
+echo "Jawaban : $c Menit";
+
+?>
---
+++ tugas3_2.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 12.5;
+$b = 150000;
+$c = $a / 100 * $b;
+$d = $b + $c;
+
+echo "Tabungan Awal = Rp $b <br>";
+echo "Tabungan Awal = Rp $b <br>";
+echo "Tabungan Awal = Rp $b <br>";
+echo "Jawaban : Rp $d <br>";
+
+?>
